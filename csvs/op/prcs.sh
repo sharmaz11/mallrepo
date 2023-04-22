62c42411aa41c7f1dc5430f810b69a65a13b51d9
sushil <sharma_aps@hotmail.com>
2023-04-22 12:26:21 +0530

    sadad

--- csvs/op/prcs.sh
+++ csvs/op/prcs.sh
@@ -5,18 +5,25 @@ done
 
 echo "................................."
 
-opn=processed.csv
+opn1=processed1.csv
+opn2=processed2.csv
+opn3=processed3.csv
 x=1
+y=1
+z=1
 for file in $(<$subd); do
 	fbn=$(basename "$file")
 	echo $fbn
 	echo $x$opn
 	x=$(( $x + 1 ))
+	y=$(( $y + 1 ))
+	z=$(( $z + 1 ))
 
-	screen -dm -S "meta"$x python3 metpathway.py $fbn $x$opn
+	screen -dm -S "meta"$x python3 metpathway.py $fbn $x$opn1 $y$opn2 $z$opn3
 done
 
 
 
 
 rm -f temp1.txt
+

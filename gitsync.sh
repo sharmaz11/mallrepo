775d9c843698a4be158f551427a42d6fe05f1443
sushil <sharma_aps@hotmail.com>
2023-04-22 18:29:58 +0530

    win20

---
+++ gitsync.sh
@@ -0,0 +1,9 @@
+
+echo "git pull"
+git pull
+echo "git add ."
+git add .
+echo "git commit ubu 2"
+git commit -m "ubu 2"
+echo "git push"
+git push
---
+++ gssync.ps1
@@ -0,0 +1,9 @@
+
+Write-Host "git pull"
+git pull
+Write-Host "git add ."
+git add .
+Write-Host "git commit win 2"
+git commit -m "win20"
+Write-Host "git push"
+git push

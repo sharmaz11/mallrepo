ff514cde154955ee96d85b585fddc888f7ac23ef
Ubuntu <malladmin@mallvm.j4wopsfp5f2exjvnibeg1gyduf.ix.internal.cloudapp.net>
2023-04-22 06:44:17 +0000

    asdad

--- csvs/op/prcs.sh
+++ csvs/op/prcs.sh
@@ -1,6 +1,22 @@
 subd=temp1.txt
 for dfile in ./*.csv; do
-	echo $dfile >> $subd
+	echo $dfile >> $subd 
 done
 
-cat $subd
+echo "................................."
+
+opn=processed.csv
+x=1
+for file in $(<$subd); do
+	fbn=$(basename "$file")
+	echo $fbn
+	echo $x$opn
+	x=$(( $x + 1 ))
+
+	screen -dm -S "meta"$x python3 metpathway.py $fbn $x$opn
+done
+
+
+
+
+rm -f temp1.txt
